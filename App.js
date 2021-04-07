04a337d5ae089a194258ab26fe8c6b3f739d47cb
Daniel Pacheco <daniel_amorimpacheco@hotmail.com>
2021-04-06 21:17:30 -0300

    formulario

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React, {Component}from 'react';
-import {StyleSheet,SafeAreaView, Text,TextInput } from 'react-native'
+import {StyleSheet,SafeAreaView, Text,TextInput, Slider, Switch, ScrollView, Button, Alert} from 'react-native'
 import {Picker} from '@react-native-picker/picker';
 
 class App extends Component{
 
+
   constructor(props){
     super(props);
 
-    nome: ''
-
-
     this.state = {
+      toggled : false,
+      sliderValue: 5,
+      nomeCompleto: '',
+      idade: '',
       curso: 0,
+      value: 0,
       cursos:[
         {key: 1, nome: 'Administração'},
         {key: 2, nome: 'Ciências Contábeis'},
@@ -31,11 +34,37 @@ class App extends Component{
         {key: 2, nome: 'Tarde'},
         {key: 3, nome: 'Noite'}
       ],
+      sexo: 0,
+      sexos:[
+        {key: 1, nome: 'Feminino'},
+        {key: 2, nome: 'Masculino'},
+      ],
     }
   }
 
+  togleSwitch = (value) => {
+    this.setState ({toggled : value})
+  }
 
   render(){
+
+    const salvar = () =>{
+      Alert.alert(
+        "Confirmação",
+        "Deseja realmente salvar?",
+        [
+          {
+            text: "Sim",
+            onPress: () => console.log("Sim")
+          },
+          {
+            text: "Não",
+            onPress: () => console.log("Não")
+          }
+        ]
+      )
+    }
+
 
     let cursoItem = this.state.cursos.map((v, k)=>{
       return <Picker.Item key={k} value={k} label={v.nome}/>
@@ -48,13 +77,24 @@ class App extends Component{
     let turnoItem = this.state.turnos.map((v, k)=>{
       return <Picker.Item key={k} value={k} label={v.nome}/>
     })
+    let sexoItem = this.state.sexos.map((v, k)=>{
+      return <Picker.Item key={k} value={k} label={v.nome}/>
+    })
 
 
     return( 
-      <SafeAreaView  >      
+      <SafeAreaView  >   
+        <ScrollView style={styles.scrollView}> 
        <Text style = {styles.tituloParametros}>Selecione os Parâmentros</Text>
        <TextInput
           placeholder = 'Digite seu nome'
+          onChangeText = {(nomeCompleto) => this.setState({nomeCompleto})}
+          value={this.state.nomeCompleto}
+      />
+       <TextInput
+          placeholder = 'Digite sua idade'
+          onChangeText = {(idade) => this.setState({idade})}
+          value={this.state.idade}
       />
        <Picker
         selectedValue={this.state.curso}
@@ -75,13 +115,42 @@ class App extends Component{
         onValueChange={(itemValue, itemIndex) => this.setState({turno: itemValue})}
         >
           {turnoItem}
-       
        </Picker>
-       <Text style = {styles.titulo}>Informações inseridas</Text>
-       <Text style = {styles.relatorio}> Nome:{this.state.nome} </Text>
-       <Text style = {styles.relatorio}> Curso: {this.state.cursos[this.state.curso].nome}</Text>
-       <Text style = {styles.relatorio}> Período: {this.state.periodos[this.state.periodo].nome}</Text>
-       <Text style = {styles.relatorio}> Turno: {this.state.turnos[this.state.turno].nome}</Text>
+
+       <Picker
+        selectedValue={this.state.sexo}
+        onValueChange={(itemValue, itemIndex) => this.setState({sexo: itemValue})}
+        >
+          {sexoItem}
+       </Picker>
+       <Text style = {styles.relatorio}>    Renda</Text>
+       <Slider
+        maximumValue={10000}
+        minimumValue={0}
+        step={1}
+        minimumTrackTintColor="red"
+        maximumTrackTintColor="blue"
+        value={this.state.sliderValue}
+        onValueChange={value => this.setState({sliderValue: value})}
+       />
+        <Text style = {styles.relatorio}>    R$ {this.state.sliderValue},00</Text>
+        
+       <Text style = {styles.relatorio}> Ganhou Bolsa: {this.state.toggled? "SIM": "NAO"} </Text>
+       <Switch onValueChange = {this.togleSwitch}
+                value = {this.state.toggled}/>
+      
+          <Text style = {styles.titulo}>Informações inseridas</Text>
+          <Text style = {styles.relatorio}> Nome: {this.state.nomeCompleto} </Text>
+          <Text style = {styles.relatorio}> Idade: {this.state.idade} </Text>
+          <Text style = {styles.relatorio}> Curso: {this.state.cursos[this.state.curso].nome}</Text>
+          <Text style = {styles.relatorio}> Período: {this.state.periodos[this.state.periodo].nome}</Text>
+          <Text style = {styles.relatorio}> Turno: {this.state.turnos[this.state.turno].nome}</Text>
+          <Text style = {styles.relatorio}> Sexo: {this.state.sexos[this.state.sexo].nome}</Text>
+          <Text style = {styles.relatorio}> Renda: R$ {this.state.sliderValue},00</Text>
+          <Text style = {styles.relatorio}> Ganhou Bolsa: {this.state.toggled? "SIM": "NAO"} </Text>
+
+          <Button title = "Salvar Informações" onPress={salvar} />
+        </ScrollView>  
       </SafeAreaView>
     )   
   }
